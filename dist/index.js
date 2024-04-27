f5986a40b15b5b6c1e873788c8121c37ef1489a0
Ben Challis <ben@lendable.co.uk>
2024-04-27 04:36:15 +0100

    Port to TS

--- dist/index.js
+++ dist/index.js
@@ -29188,6 +29188,163 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 2100:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.colors = void 0;
+exports.colors = {
+    reset: "\x1b[0m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+};
+
+
+/***/ }),
+
+/***/ 653:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.delay = void 0;
+async function delay(secs) {
+    return new Promise((resolve) => setTimeout(resolve, secs * 1000));
+}
+exports.delay = delay;
+
+
+/***/ }),
+
+/***/ 6144:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
+const colors_1 = __nccwpck_require__(2100);
+const delay_1 = __nccwpck_require__(653);
+const startTime = new Date();
+const inputs = {
+    name: core.getInput("name"),
+    interval: Number(core.getInput("interval")),
+    timeout: Number(core.getInput("timeout")),
+    ref: core.getInput("ref"),
+    ignored: new Set(core.getMultilineInput("ignored")),
+};
+const octokit = github.getOctokit(core.getInput("token", { required: true }));
+const failureConclusions = ["failure", "cancelled", "timed_out"];
+if (inputs.ignored.size > 0) {
+    console.info("::group::Ignored check names");
+    console.info([...inputs.ignored]);
+    console.info("::endgroup::");
+}
+const outputCheckRuns = (icon, color, runs) => {
+    if (runs.length === 0) {
+        return;
+    }
+    console.info(`::group::${icon} ${color}${runs.length}${colors_1.colors.reset}`);
+    for (const name of runs) {
+        console.info(name);
+    }
+    console.info("::endgroup::");
+};
+(async () => {
+    while (true) {
+        let checks = [];
+        console.info("");
+        const iterator = octokit.paginate.iterator(octokit.rest.checks.listForRef, {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            ref: inputs.ref,
+            per_page: 100,
+        });
+        for await (const { data } of iterator) {
+            checks = checks.concat(data);
+        }
+        checks = checks.filter((v) => v.name !== inputs.name && !inputs.ignored.has(v.name));
+        core.debug(`Found a total of ${checks.length} relevant check runs`);
+        if (checks.length === 0) {
+            console.info(`Slothing, verifying again in ${inputs.interval}s...`);
+            await (0, delay_1.delay)(inputs.interval);
+            continue;
+        }
+        const pending = [];
+        const failures = [];
+        const successful = [];
+        for (const check of checks) {
+            if (!check.conclusion) {
+                pending.push(check.name);
+            }
+            else {
+                if (failureConclusions.includes(check.conclusion)) {
+                    failures.push(check.name);
+                }
+                else {
+                    successful.push(check.name);
+                }
+            }
+        }
+        for (const runs of [successful, failures, pending]) {
+            runs.sort();
+        }
+        outputCheckRuns("✅", colors_1.colors.green, successful);
+        outputCheckRuns("❌", colors_1.colors.red, failures);
+        outputCheckRuns("⏳", colors_1.colors.reset, pending);
+        if (failures.length > 0) {
+            console.info("");
+            console.info(`❗ ${colors_1.colors.red}Failure!${colors_1.colors.reset}`);
+            core.setFailed("A check run failed.");
+            return;
+        }
+        if (pending.length === 0) {
+            console.info("");
+            console.info(`🚀 ${colors_1.colors.green}Success!${colors_1.colors.reset}`);
+            return;
+        }
+        const executionTime = Math.round((new Date().getTime() - startTime.getTime()) / 1000);
+        if (executionTime > inputs.timeout) {
+            console.info("");
+            console.info(`⏰ ${colors_1.colors.red}Timed out!${colors_1.colors.reset}`);
+            core.setFailed("Timed out waiting on check runs to all be successful.");
+            return;
+        }
+        console.info(`Slothing, verifying again in ${inputs.interval}s...`);
+        await (0, delay_1.delay)(inputs.interval);
+    }
+})();
+
+
+/***/ }),
+
 /***/ 9491:
 /***/ ((module) => {
 
@@ -31075,132 +31232,12 @@ module.exports = parseParams
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-const core = __nccwpck_require__(2186);
-const github = __nccwpck_require__(5438);
-
-async function delay(secs) {
-  return new Promise((resolve) => setTimeout(resolve, secs * 1000));
-}
-
-const startTime = new Date();
-
-const inputs = {
-  name: core.getInput('name'),
-  interval: core.getInput('interval'),
-  timeout: core.getInput('timeout'),
-  ref: core.getInput('ref'),
-  ignored: new Set(core.getMultilineInput('ignored')),
-};
-
-const octokit = github.getOctokit(core.getInput('token', { required: true }));
-
-const colors = {
-  reset: '\x1b[0m',
-  red: '\x1b[31m',
-  green: '\x1b[32m',
-};
-if (inputs.ignored.size > 0) {
-  console.info('::group::Ignored check names');
-  console.info([...inputs.ignored]);
-  console.info('::endgroup::');
-}
-
-const run = async () => {
-  while (true) {
-    let checks = [];
-
-    console.info('');
-
-    const iterator = octokit.paginate.iterator(
-      octokit.rest.checks.listForRef,
-      {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        ref: inputs.ref,
-        per_page: 100,
-      },
-    );
-
-    for await (const checkRun of iterator) {
-      checks = checks.concat(checkRun.data);
-    }
-
-    checks = checks.filter((v) => v.name !== inputs.name && !inputs.ignored.has(v.name));
-
-    core.debug(`Found a total of ${checks.length} relevant check runs`);
-
-    if (checks.length === 0) {
-      console.info(`Slothing, verifying again in ${inputs.interval}s...`);
-      await delay(inputs.interval);
-      continue;
-    }
-
-    const pending = [];
-    const failures = [];
-    const successful = [];
-
-    checks.forEach((check) => {
-      if (!check.conclusion) {
-        pending.push(check.name);
-      } else if (['failure', 'cancelled'].includes(check.conclusion)) {
-        failures.push(check.name);
-      } else {
-        successful.push(check.name);
-      }
-    });
-
-    [successful, failures, pending].forEach((list) => list.sort());
-
-    if (successful.length > 0) {
-      console.info(`::group::✅ ${colors.green}${successful.length}${colors.reset}`);
-      successful.forEach((v) => console.info(v));
-      console.info('::endgroup::');
-    }
-
-    if (failures.length > 0) {
-      console.info(`::group::❌ ${colors.red}${failures.length}${colors.reset}`);
-      failures.forEach((v) => console.info(v));
-      console.info('::endgroup::');
-    }
-
-    if (pending.length > 0) {
-      console.info(`::group::⏳ ${pending.length}`);
-      pending.forEach((v) => console.info(v));
-      console.info('::endgroup::');
-    }
-
-    if (failures.length > 0) {
-      console.info('');
-      console.info(`❗ ${colors.red}Failure!${colors.reset}`);
-      core.setFailed('A check run failed.');
-      return;
-    }
-
-    if (pending.length === 0) {
-      console.info('');
-      console.info(`🚀 ${colors.green}Success!${colors.reset}`);
-      return;
-    }
-
-    if (Math.round((new Date() - startTime) / 1000) > inputs.timeout) {
-      console.info('');
-      console.info(`⏰ ${colors.red}Timed out!${colors.reset}`);
-      core.setFailed('Timed out waiting on check runs to all be successful.');
-      return;
-    }
-
-    console.info(`Slothing, verifying again in ${inputs.interval}s...`);
-    await delay(inputs.interval);
-  }
-};
-
-run();
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(6144);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
